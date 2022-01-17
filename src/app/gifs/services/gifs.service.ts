5769169e3f20ddca56930f57234439a929a8015f
Jhonatan Ramos <cram2686@gmail.com>
2022-01-17 08:04:01 -0600

    Tipado de peticiones http

--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Gifs, SearchGIFResponse } from '../interface/gifs.interface';
 
 @Injectable({
   providedIn: 'root'
@@ -10,7 +11,7 @@ export class GifsService {
   private _apiKey   : string = '2PmbzWsUbXDBeCvQLxNCCUgWgphgspOA';
   private _historial: string[] = [];
 
-  public resultados: any[] = [];
+  public resultados: Gifs[] = [];
 
   get historial() {
     return [...this._historial];
@@ -30,8 +31,8 @@ export class GifsService {
       this._historial = this._historial.splice(0,10);
     }
 
-    this.http.get(`https://api.giphy.com/v1/gifs/search?api_key=2PmbzWsUbXDBeCvQLxNCCUgWgphgspOA&q=${ query }&limit=10`)
-      .subscribe ( ( resp: any ) => {
+    this.http.get<SearchGIFResponse>(`https://api.giphy.com/v1/gifs/search?api_key=2PmbzWsUbXDBeCvQLxNCCUgWgphgspOA&q=${ query }&limit=10`)
+      .subscribe ( ( resp ) => {
         console.log(resp.data);
         this.resultados = resp.data;
       })
